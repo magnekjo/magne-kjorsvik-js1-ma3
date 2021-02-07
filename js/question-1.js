16959426588221d3757a7121b54a9742ae8f3bd5
magnekjo <71463184+magnekjo@users.noreply.github.com>
2021-02-07 21:03:01 +0100

    Added everything

---
+++ js/question-1.js
@@ -0,0 +1,5 @@
+// Question 1
+
+const division = (a, b) => console.log(a % b);
+
+division(50, 6);
---
+++ js/question-2.js
@@ -0,0 +1,32 @@
+// Question 2
+
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+const gamesContainer = document.querySelector(".gamesContainer");
+
+async function getGames() {
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+    const gamesInfo = results.results;
+
+    gamesContainer.innerHTML = "";
+
+    for (let i = 0; i < gamesInfo.length; i++) {
+      //console.log(gamesInfo[i].name);
+      //console.log(gamesInfo[i].rating);
+
+      gamesContainer.innerHTML += `<div class="game">Name: ${gamesInfo[i].name}<br>Rating: ${gamesInfo[i].rating}<br>Tags: ${gamesInfo[i].tags.length} </div>`;
+
+      if (i === 8) {
+        break;
+      }
+    }
+  } catch (error) {
+    console.log("An error has occured");
+    gamesContainer.innerHTML = "An error has occured. " + error;
+  }
+}
+
+getGames();
